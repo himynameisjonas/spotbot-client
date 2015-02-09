6c11b26deaddcb64e7c66335c0a81069c0e65a4b
Jonas Brusman <himynameisjonas@gmail.com>
2015-02-09 22:20:02 +0100

    Change playlist

--- app/components/spotify-playlist.js
+++ app/components/spotify-playlist.js
@@ -1,12 +1,18 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Component.extend({
   tracksData: [],
+  newPlaylistUri: null,
 
   fetchSpotifyData: function(){
-    Ember.$.get("https://api.spotify.com/v1/tracks/", {ids: this.get("spotifyIds").join(",")}).then((data)=> {
-      this.get("tracksData").pushObjects(data.tracks);
-    });
+    var array = this.get("spotifyIds");
+    var i,j,temparray,chunk = 50;
+    for (i=0,j=array.length; i<j; i+=chunk) {
+      temparray = array.slice(i,i+chunk);
+      Ember.$.get("https://api.spotify.com/v1/tracks/", {ids: temparray.join(",")}).then((data)=> {
+        this.get("tracksData").pushObjects(data.tracks);
+      });
+    }
   }.observes("data.tracks.[]"),
 
 
@@ -15,4 +21,17 @@ export default Ember.Component.extend({
       return item.split(":").slice(-1)[0];
     });
   }.property("data.tracks.[]"),
+
+  ref: function(){
+    return this.store.adapterFor("application").get("_ref");
+  }.property("store"),
+
+  actions: {
+    newPlaylist: function(){
+      var uri = this.get("newPlaylistUri");
+      if (!Ember.isEmpty(uri)) {
+        this.get("ref").child("playlist/uri").set(uri);
+      }
+    },
+  }
 });
