57a82c6e633c5bbdf8c20ce1403594abd7c6ad9c
Jonas Brusman <himynameisjonas@gmail.com>
2015-02-09 20:53:21 +0100

    Control Play/Pause/Next via Firebase

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,26 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Controller.extend({
   query: null,
+  playing: false,
+
+  init: function(){
+    this.get("playingRef").on("value", (data)=>{
+      if (data.val() === true) {
+        this.set("playing", true);
+      } else if (data.val() === false) {
+        this.set("playing", false);
+      }
+    });
+  },
+
+  ref: function(){
+    return this.store.adapterFor("application").get("_ref");
+  }.property(),
+
+  playingRef: function(){
+    return this.get("ref").child("player/playing");
+  }.property("ref"),
 
   actions: {
     search: function(){
@@ -10,6 +29,18 @@ export default Ember.Controller.extend({
       } else {
         this.transitionToRoute("search", {query: this.get("query")});
       }
-    }
+    },
+    play: function(){
+      this.get("playingRef").set(true);
+    },
+    pause: function(){
+      this.get("playingRef").set(false);
+    },
+    next: function(){
+      this.get("ref").child("player/next").set(true);
+    },
+    shuffle: function(){
+
+    },
   }
 });
