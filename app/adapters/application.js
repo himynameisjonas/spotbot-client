dabaac61448cecf005edfe8ab8014f9da2d0cf65
Jonas Brusman <himynameisjonas@gmail.com>
2015-02-10 07:56:16 +0100

    Read firebase url from env

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,8 @@
 /* globals Firebase */
 import DS from 'ember-data';
+import config from 'spotbot-client/config/environment';
+
 
 export default DS.FirebaseAdapter.extend({
-  firebase: new Firebase("https://spotobot-dev.firebaseio.com")
+  firebase: new Firebase(config.FIREBASE_URL)
 });
--- config/environment.js
+++ config/environment.js
@@ -25,6 +25,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.FIREBASE_URL = "https://spotobot-dev.firebaseio.com"
   }
 
   if (environment === 'test') {
@@ -40,7 +41,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.FIREBASE_URL = process.env.FIREBASE_URL
   }
 
   return ENV;
